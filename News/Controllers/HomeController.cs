91f44bd4b504fd4be69ffaf87718ca7c5125f84d
stu <stu@qq.com>
2016-10-19 11:51:50 +0800

    添加一个简单的新闻列表

--- News/Controllers/HomeController.cs
+++ News/Controllers/HomeController.cs
@@ -26,5 +26,16 @@ namespace News.Controllers
 
             return View();
         }
+
+        public ActionResult NewsList()
+        {
+            string[] data = new string[] { "中纪委纪录片第二集：“老虎”万庆良谷春立出镜(图)",
+                "杜特尔特：执行“南海仲裁”决定或引第三次世界大战",
+                "武汉造“防洪神器”亮相 拦水高度可达30米(图)",
+                "伊拉克军继续挺进摩苏尔 发现多条IS地道" };
+            ViewBag.data = data;
+            return View();
+        }
+
     }
 }
