6be9e667b2e2fc2afe43eadb8c9ff72f20f4e3a0
1351300220 <1351300220@qq.com>
2016-11-09 10:39:00 +0800

    Cookies简单操作

---
+++ News/Controllers/CookiesController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace News.Controllers
+{
+    public class CookiesController : Controller
+    {
+        // GET: Cookies
+        public ActionResult Index()
+        {
+            int count = 1;
+            var cookies = Request.Cookies["count"];
+            if(cookies == null)
+            {
+                Response.Cookies.Add(new HttpCookie("count","1"));
+            }
+            else
+            {
+                count = int.Parse(cookies.Value) + 1;
+                var newcookies = new HttpCookie("count",count.ToString());
+                newcookies.Expires = DateTime.Now.AddMinutes(20);
+                Response.Cookies.Add(newcookies);
+            }
+            ViewBag.count = count;
+            return View();
+        }
+    }
+}
